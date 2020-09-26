d68be8afcf8f45f6e4cbf6f45d40b11385e7d4ab
AliBaran GUNDUZ <alibarangunduzz@gmail.com>
2020-09-26 15:04:43 +0300

    working on pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { JumbotronContainer } from './containers/jumbotron';
-import { FooterContainer } from './containers/footer';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Home, Browse, SignIn, SignUp } from "./pages";
+import * as ROUTES from './constants/routes';
+
 export default function App() {
-  return (
-    <>
-      <JumbotronContainer />
-      <FooterContainer />
-    </>
-  );
+  
+    
+      return (
+        <Router>
+          <Route exact path="/browse">
+            <Browse />
+          </Route>
+          <Route exact path="/signin">
+            <SignIn />
+          </Route>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+          <Route exact path={ROUTES.HOME}>
+            <Home />
+          </Route>
+        </Router>
+      );
 };
 
 
