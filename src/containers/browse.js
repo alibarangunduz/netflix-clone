0c73fda630dec1d7475a7401a3405eef6a4eb562
AliBaran GUNDUZ <alibarangunduzz@gmail.com>
2020-09-30 19:35:18 +0300

    working on browse page

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,75 +1,127 @@
-import React, { useState, useContext, useEffect} from 'react';
-import {SelectProfileContainer} from './profiles'
-import { FirebaseContext } from '../context/firebase';
-import { Header, Loading } from '../components'
+import React, { useState, useEffect, useContext } from "react";
+import Fuse from "fuse.js";
+import { Card, Header, Loading, Player } from "../components";
 import * as ROUTES from "../constants/routes";
-import logo from '../logo.svg';
+import logo from "../logo.svg";
+import { FirebaseContext } from "../context/firebase";
+import { SelectProfileContainer } from "./profiles";
+import { FooterContainer } from "./footer";
 
 export function BrowseContainer({ slides }) {
-    const [searchTerm, setSearchTerm] = useState('')
-    const [profile, setProfile] = useState({}); 
-    const [loading, setLoading] = useState(true);
-    const { firebase } = useContext(FirebaseContext);
+  const [category, setCategory] = useState("series");
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [slideRows, setSlideRows] = useState([]);
 
-    const user = firebase.auth().currentUser || {};
-    
-    useEffect(() => {
-        console.log('profile', profile)
-        setTimeout(() => {
-           setLoading(false); 
-        }, 3000);
-    }, [profile.displayName]);
+  const { firebase } = useContext(FirebaseContext);
+  const user = firebase.auth().currentUser || {};
 
-    return profile.displayName ? (
-      <>
-        {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
 
-        <Header src="joker1" dontShowOnSmallViewPort>
-          <Header.Frame>
-            <Header.Group>
-              <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
-              <Header.TextLink>Series</Header.TextLink>
-              <Header.TextLink>Films</Header.TextLink>
-            </Header.Group>
-            <Header.Group>
-              <Header.Search
-                searchTerm={searchTerm}
-                setSearchTerm={setSearchTerm}
-              />
-              <Header.Profile>
-                <Header.Picture src={user.photoURL} />
-                <Header.Dropdown>
-                  <Header.Group>
-                    <Header.Picture src={user.photoURL} />
-                    <Header.TextLink>{user.displayName}</Header.TextLink>
-                  </Header.Group>
-                  <Header.Group>
-                    <Header.TextLink onClick={() => firebase.auth().signOut()}>
-                      Sign Out
-                    </Header.TextLink>
-                  </Header.Group>
-                </Header.Dropdown>
-              </Header.Profile>
-            </Header.Group>
-          </Header.Frame>
-          <Header.Feature>
-            <Header.FeatureCallOut> Watch Joker Now</Header.FeatureCallOut>
-            <Header.Text>
-              The Bishop of Hong Kong is the head of the Roman Catholic Diocese
-              of Hong Kong, and responsible for looking after its spiritual and
-              administrative needs. The Diocese of Hong Kong is nominally part
-              of the ecclesiastical province of Canton and thus is a suffragan
-              of that archdiocese. However, it has been exempt in practice since
-              1951, when the People's Republic of China severed diplomatic
-              relations with the Holy See. As a crown colony of the United
-              Kingdom at the time
-            </Header.Text>
-            <Header.PlayButton>Play</Header.PlayButton>
-          </Header.Feature>
-        </Header>
-      </>
-    ) : (
-      <SelectProfileContainer user={user} setProfile={setProfile} />
-    ); 
-    
+  useEffect(() => {
+    setSlideRows(slides[category]);
+  }, [slides, category]);
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ["data.description", "data.title", "data.genre"],
+    });
+    const results = fuse.search(searchTerm).map(({ item }) => item);
+
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
+
+  return profile.displayName ? (
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+
+      <Header src="joker1" dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+            <Header.TextLink
+              active={category === "series" ? "true" : "false"}
+              onClick={() => setCategory("series")}
+            >
+              Series
+            </Header.TextLink>
+            <Header.TextLink
+              active={category === "films" ? "true" : "false"}
+              onClick={() => setCategory("films")}
+            >
+              Films
+            </Header.TextLink>
+          </Header.Group>
+          <Header.Group>
+            <Header.Search
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+            />
+            <Header.Profile>
+              <Header.Picture src={user.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.Picture src={user.photoURL} />
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                  <Header.TextLink onClick={() => firebase.auth().signOut()}>
+                    Sign out
+                  </Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
+
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks
+            connection as he walks the streets of Gotham City. Arthur wears two
+            masks -- the one he paints for his day job as a clown, and the guise
+            he projects in a futile attempt to feel like he's part of the world
+            around him.
+          </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
+        </Header.Feature>
+      </Header>
+
+      <Card.Group>
+        {slideRows.map((slideItem) => (
+          <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+            <Card.Title>{slideItem.title}</Card.Title>
+            <Card.Entities>
+              {slideItem.data.map((item) => (
+                <Card.Item key={item.docId} item={item}>
+                  <Card.Image
+                    src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
+                  />
+                  <Card.Meta>
+                    <Card.SubTitle>{item.title}</Card.SubTitle>
+                    <Card.Text>{item.description}</Card.Text>
+                  </Card.Meta>
+                </Card.Item>
+              ))}
+            </Card.Entities>
+            <Card.Feature category={category}>
+           
+            </Card.Feature>
+          </Card>
+        ))}
+      </Card.Group>
+      <FooterContainer />
+    </>
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
